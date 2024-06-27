b6be97c0daa624104466683e7d204e87d7a5dc97
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-06-27 04:31:52 -0300

    feat(infra): allow traffic outbound

--- infra/security.tf
+++ infra/security.tf
@@ -4,4 +4,11 @@ resource "aws_security_group" "fssou" {
   name        = "fssou"
   description = "Security group for fssou API."
   vpc_id      = data.aws_vpc.default.id
+  egress {
+    from_port = -1
+    to_port   = -1
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all traffic out."
+  }
 }

37a91711583d9d92c75b20a22560b1ec4b3c4a11
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-06-29 01:49:02 -0300

    fix: ipv6 cidr blocks

--- infra/security.tf
+++ infra/security.tf
@@ -8,14 +8,16 @@ resource "aws_security_group" "fssou" {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
     description = "Allow all traffic out."
   }
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
     description = "Allow all traffic out."
   }
 }

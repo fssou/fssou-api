085897bb3d1f2c6d551150f4fcf7af6c55e70ecb
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-07-01 23:55:31 -0300

    feat(infra): rename secret

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,6 +1,6 @@
 # secrets.tf
 
 resource "aws_secretsmanager_secret" "x_credentials" {
-  name = "fssou-x-credentials"
+  name = "fssou-api-x-credentials"
   description = "Secret for fssou x credentials."
 }

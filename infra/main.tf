d913a3d97bc0314becd3726422a3a05c83b37fa3
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-07-05 23:11:46 -0300

    build: add cors

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,15 @@
 # main.tf
 
 resource "aws_lambda_function" "fssou" {
-  function_name = local.function_name
-  description   = "This is a function for fssou API."
-  handler       = "bootstrap"
-  filename      = "fssou.zip"
-  runtime       = "provided.al2023"
-  timeout       = 15
-  publish       = true
+  function_name    = local.function_name
+  description      = "This is a function for fssou API."
+  handler          = "bootstrap"
+  filename         = "fssou.zip"
+  runtime          = "provided.al2023"
+  timeout          = 15
+  publish          = true
   source_code_hash = fileexists("fssou.zip") ? filebase64sha256("fssou.zip") : null
-  role          = aws_iam_role.lambda.arn
+  role             = aws_iam_role.lambda.arn
   architectures = ["x86_64"]
   environment {
     variables = {
@@ -30,4 +30,12 @@ resource "aws_lambda_alias" "fssou" {
 resource "aws_lambda_function_url" "fssou" {
   authorization_type = "NONE"
   function_name      = aws_lambda_function.fssou.function_name
+  cors {
+    max_age           = 3000
+    allow_credentials = true
+    expose_headers = ["*"]
+    allow_origins = ["*"]
+    allow_methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    allow_headers = ["*"]
+  }
 }
